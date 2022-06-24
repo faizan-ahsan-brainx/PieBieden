0d8764fe7b049be07d647472058955c573be7e8a
Faizan Ahsan <faizan.ahsan@brainxtech.com>
2022-06-24 12:01:47 +0500

    SignUp and SignIn Completed

---
+++ PieBiden/Globals/LocalizeableKeys.swift
@@ -0,0 +1,14 @@
+//
+//  LocalizeableKeys.swift
+//  PieBiden
+//
+//  Created by BrainX on 17/06/2022.
+//
+
+import Foundation
+enum LocalizableKey: String {
+    var string: String  {
+        return self.rawValue
+    }
+    case sectionLabel = "Languages"
+}
---
+++ PieBiden/Views/SignUpView.swift
@@ -0,0 +1,79 @@
+//
+//  SignUpView.swift
+//  PieBiden
+//
+//  Created by BrainX on 16/06/2022.
+//
+
+import UIKit
+
+class SignUpView: UIView {
+    
+    
+    @IBOutlet weak var signUpLabel: UILabel!
+    @IBOutlet weak var termsAndPlolicyLabel: UILabel!
+    @IBOutlet weak var signUpAppleButton: UIButton!
+    @IBOutlet weak var signUpFacebookButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var alreadyMemberLabel: UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var phoneView: UIView!
+    
+    
+    func configureOutlets() {
+        signUpLabel.font = .systemFont(ofSize: 24, weight: .medium)
+        
+        signUpAppleButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .medium)
+        signUpAppleButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+        signUpAppleButton.layer.shadowOffset = CGSize(width: 10.0, height: 6.0)
+        signUpAppleButton.layer.shadowOpacity = 1.0
+        signUpAppleButton.layer.shadowRadius = 5.0
+        signUpAppleButton.layer.masksToBounds = false
+        
+        signUpFacebookButton.titleLabel?.font = .systemFont(ofSize: 14, weight: .medium)
+        signUpFacebookButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.15).cgColor
+        signUpFacebookButton.layer.shadowOffset = CGSize(width: 10.0, height: 6.0)
+        signUpFacebookButton.layer.shadowOpacity = 1.0
+        signUpFacebookButton.layer.shadowRadius = 5.0
+        signUpFacebookButton.layer.masksToBounds = false
+        
+        configureTextFieldsBorder()
+        
+        signUpButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        
+        alreadyMemberLabel.font = .systemFont(ofSize: 16, weight: .medium)
+        signInButton.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        signInButton.tintColor = UIColor(rgb: 0x4146D6)
+        
+        phoneView.layer.borderWidth = 1
+        phoneView.layer.borderColor = UIColor(rgb: 0xCCCCCC).cgColor
+        phoneView.layer.cornerRadius = 5
+        phoneView.frame.inset(by: UIEdgeInsets(top: 30.0, left: 5.0, bottom: 30.0, right: 5.0))
+        configureTermsAndPolicyLabel()
+        //UIEdgeInsets(top: 30, left: 10, bottom: 30, right: 10);
+    }
+    func configureTermsAndPolicyLabel(){
+        let fullText = "By clicking Sign up, Continue with Facebook or Continue with Apple, you agree to our Terms and Conditions and Privacy Policy."
+        let changeText1 = "Terms and Conditions"
+        let changeText2 = "Privacy Policy."
+        
+        let strNumber: NSString = fullText as NSString
+        var range = (strNumber).range(of: changeText1)
+        var attribute = NSMutableAttributedString.init(string: fullText)
+        attribute.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(rgb: 0xFC7941) , range: range)
+        range = (strNumber).range(of: changeText2)
+        attribute = NSMutableAttributedString.init(string: attribute.string)
+        attribute.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(rgb: 0xFC7941), range: range)
+        termsAndPlolicyLabel.attributedText = attribute
+        termsAndPlolicyLabel.font = UIFont.systemFont(ofSize: 11)
+    }
+    
+    func configureTextFieldsBorder(){
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: 7.0, width: 1.0, height: phoneTextField.frame.height)
+        bottomLine.backgroundColor = UIColor(rgb: 0xCCCCCC).cgColor
+        phoneTextField.layer.addSublayer(bottomLine)
+        phoneTextField.borderStyle = UITextField.BorderStyle.none
+    }
+}
